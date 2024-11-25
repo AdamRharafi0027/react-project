5180aec902086bce2ffe848950043b73e7687051
Adam Rharafi <adamrharafi.dev@gmail.com>
2024-11-25 11:59:41 +0100

    reactC

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react-project/',
   plugins: [react()],
+  base: '/react-project/', // Replace 'your-repo-name' with the actual repository name
+  build: {
+    outDir: 'build', // Change the output directory to 'build'
+  },
 })
 
 
